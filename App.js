91d6056e6e4b71493770e58251c770b4a9b05825
snsalifu123 <salifushahaban@icloud.com>
2021-11-01 03:18:32 +0000

    Third commit

--- App.js
+++ App.js
@@ -1,35 +1,8 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import Login from "./screen/Login";
-import Home from "./screen/Home";
+
+// screen
+import Login from './screen/Login';
 
 export default function App() {
-  const MainNavigator = createStackNavigator();
-
-  return (
-    <View style={{ flex: 1 }}>
-      <NavigationContainer>
-        <MainNavigator.Navigator
-          screenOptions={{ headerShown: true ,  }}
-          // initialRouteName=""
-        >
-          <MainNavigator.Screen name="Login" component={Login} />
-          <MainNavigator.Screen name="Home" component={Home} />
-         
-        </MainNavigator.Navigator>
-      </NavigationContainer>
-    </View>
-  );
+  return <Login />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- screen/Login.js
+++ screen/Login.js
@@ -1,84 +1,72 @@
-import React from "react";
-import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
-import {formik} from 'formik';
+import React from 'react';
+import { StatusBar } from "expo-status-bar";
 
-export default function Login(params) {
-  const navigation = params.navigation;
-  return (
-    <View
-      style={{
-        backgroundColor: "yellow",
-        flex: 1,
-        paddingHorizontal: 20,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Image
-        style={{
-          transform: [{ rotate: "360deg" }],
-          borderRadius: 20,
-          marginBottom: 60,
-          width: 100,
-          height: 100,
-        }}
-        source={{
-          uri: "https://media.istockphoto.com/photos/bicycle-logos-picture-id172194126?s=612x612",
-        }}
-      />
-      <Text style={{ fontSize: 20, color: "black", fontFamily: " cursive" }}>Welcome to</Text>
-      <Text style={{ fontSize: 30, fontWeight: "bold", fontFamily: "Times New Roman" }}>MyBike Online Shop</Text>
-      <Text style={{ fontSize: 20, color: "purple", fontWeight:"light",}}>Account Login</Text>
-      
-      { <View style={{marginTop: 10,marginBottom: 10,}}>
-        <TextInput
-          style={{
-            padding: 15,
-            borderRadius: 5,
-            borderWidth: 0.7,
-            paddingHorizontal: 70,
-            paddingLeft: 20,
-          }}
-          placeholder="Enter your Email"
-        />
-      </View> }
-      { <View style={{ marginTop: 10,}}>
-        <TextInput
-          secureTextEntry={true}
-          onChangeText={(
-            e
-          )=> {
-            console.log(e)
-          }}
-          style={{
-            padding: 15,
-            borderRadius: 5,
-            borderWidth: 0.7,
-            paddingHorizontal: 50,
-            paddingLeft: 20,
-          }}
-          placeholder="Enter your password"
-        />
-      </View> }
-      <TouchableOpacity 
-        onPress={() => {
-          navigation.navigate("Home");
-        }}
-        style={{
-          padding: 15,
-          paddingHorizontal: 80,
-          marginTop: 10,
-          alignItems: "center",
-          borderRadius: 5,
-          flexDirection: "row",
-          backgroundColor: "green"
-          
-        }}
-      >
-        
-        <Text style={{ paddingLeft: 10, color: "black", fontSize: "15" }}>LOGIN</Text>
-      </TouchableOpacity>
-    </View>
-  );
+import { Formik } from 'formik';
+import {Octicons} from '@expo/vector-icons';
+
+import{
+    StyledContainer,
+    InnerContainer,
+    PageLogo,
+    PageTitle,
+    SubTitle,
+    StyledFormArea,
+    LeftIcon, 
+    StyledInputLabel,
+    StyledTextInput,
+    RightIcon,
+    Colors,
+} from './../components/styles';
+import { View } from 'react-native';
+
+const{brand, darkLight} = Colors;
+
+const Login = () => {
+    return (
+        <StyledContainer>
+            <StatusBar style='dark' />
+            <InnerContainer>
+                <PageLogo resizeMode="cover" source={require('./../assets/Logo.jpg')}/>
+                <PageTitle>MyBike shop</PageTitle>
+                <SubTitle>Account Login</SubTitle>
+
+                <Formik
+                    initialValues={{email: '', password:''}}
+                    onSubmit={(values) => {
+                        console.log(values);
+                    }}
+                >
+                    {({handleChange, handleBlur, handleSubmit, values})=> (
+                        <StyledFormArea>
+                          <MyTextInput
+                            label='Email Address'
+                            icon ='mail'
+                            placeholder='snsalifu@gmail.com'
+                            placeholderTextColor={darkLight}
+                            onChangeText={handleChange('email')}
+                            onBlur={handleBlur('email')}
+                            value={values.email}
+                            keyboardType={"email-address"}
+                          />
+                        </StyledFormArea>
+                    )}
+                </Formik>
+            </InnerContainer>
+        </StyledContainer>
+    );
 }
+
+const MyTextInput =(label,icon, ...props) =>{
+    return(
+        <View>
+            <LeftIcon>
+                <Octicons name={icon} size={30} color={brand}  />
+            </LeftIcon>
+            <StyledInputLabel {...props} />
+        </View>
+    )
+}
+
+export default Login;
+
+
